e15883952da637f8a54858af9d62a1b2fe571072
Tewodros Wolde <twolde01@gmail.com>
2020-04-07 21:32:11 -0700

    added leetcode 30 day challenge

---
+++ Leetcode30DayChallenge/HappyNumber.java
@@ -0,0 +1,21 @@
+public class HappyNumber {
+	public boolean isHappy(int n) {
+		int slow = n, fast = getSquare(n);
+		while(fast != 1 && slow != fast) {
+        	slow = getSquare(slow);
+        	fast = getSquare(getSquare(fast));
+        };
+        
+        return fast == 1;
+    }
+	
+	private int getSquare(int n) {
+		int sum = 0;
+		while(n > 0) {
+			int d = n % 10;
+			n /= 10;
+			sum += d * d;
+		}
+		return sum;
+	}
+}
