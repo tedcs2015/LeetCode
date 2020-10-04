f51851fc1becda6bdf68d0e1d92442fffd98731c
Tewodros Wolde <twolde01@gmail.com>
2020-10-04 16:31:54 -0700

    updated lru cache file

--- Design/LRUCache.java
+++ Design/LRUCache.java
@@ -5,17 +5,6 @@ import java.util.Map;
 
 /**
  * Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
-
-	Implement the LRUCache class:
-	
-	LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
-	int get(int key) Return the value of the key if the key exists, otherwise return -1.
-	void put(int key, int value) Update the value of the key if the key exists. Otherwise, 
-	add the key-value pair to the cache. If the number of keys exceeds the capacity from 
-	this operation, evict the least recently used key.
-	
-	Follow up:
-	Could you do get and put in O(1) time complexity?
  * 
  * Time complexity : O(1) both for put and get.
  * Space complexity : O(capacity) since the space is used only for a hashmap and
@@ -114,25 +103,5 @@ public class LRUCache {
 			this.key = key;
 			this.value = value;
 		}
-	}
-	
-	public static void main(String[] args) {
-		LRUCache lRUCache = new LRUCache(2);
-		lRUCache.put(1, 1); // cache is {1=1}
-		lRUCache.put(2, 2); // cache is {1=1, 2=2}
-//		lRUCache.get(1);    // return 1
-		System.out.println(lRUCache.get(1));
-
-		lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-		lRUCache.get(2);    // returns -1 (not found)
-		System.out.println(lRUCache.get(2));
-
-		lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-//		lRUCache.get(1);    // return -1 (not found)
-//		lRUCache.get(3);    // return 3
-//		lRUCache.get(4);    // return 4
-		System.out.println(lRUCache.get(1));
-		System.out.println(lRUCache.get(3));
-		System.out.println(lRUCache.get(4));
 	}
 }
